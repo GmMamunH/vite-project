a45bc441e8113403f331bbc3fe6f39202c41e8d0
MD MAMUN HOSSAIN <gmmamun3004@gmail.com>
2024-01-18 21:38:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Hero from './assets/component/Hero'
+import Navbar from './assets/component/Navbar'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/component/Hero.jsx
@@ -0,0 +1,11 @@
+
+
+const Hero = () => {
+    return (
+        <div>
+            <h1>mamun</h1>
+        </div>
+    );
+};
+
+export default Hero;
